b4c0c39bb60709adbe25b2b990a0c64bff01e5f1
Yogi <yogeshshankarkala@gmail.com>
2024-08-24 10:47:23 +0530

    Today

---
+++ Sample/src/arraysconcept/OneDimensionalArray.java
@@ -0,0 +1,22 @@
+package arraysconcept;
+
+public class OneDimensionalArray {
+	public static void main(String[] args) {
+		int[] a = new int[5];
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		a[4] = 5;
+//		int[] a = {1,2,3,4,5};
+		for(int i=0;i<=a.length-1;i++) {
+			System.out.println(a[i]);
+		}
+		
+		
+		  
+		for(int b:a) {
+			System.out.println(b);
+		}
+	}
+}
---
+++ Sample/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yoges
+ *
+ */
+module Sample {
+}
