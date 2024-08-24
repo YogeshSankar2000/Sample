7cb8f0ac74af6ef9d628dbe2ba3e1f88fc0c3d1e
Yogi <yogeshshankarkala@gmail.com>
2024-08-24 11:06:19 +0530

    Y

--- Sample/src/arraysconcept/OneDimensionalArray.java
+++ Sample/src/arraysconcept/OneDimensionalArray.java
@@ -7,14 +7,9 @@ public class OneDimensionalArray {
 		a[1] = 2;
 		a[2] = 3;
 		a[3] = 4;
-		a[4] = 5;
-//		int[] a = {1,2,3,4,5};
-		for(int i=0;i<=a.length-1;i++) {
+		for (int i = 0; i <= a.length - 1; i++) {
 			System.out.println(a[i]);
 		}
-		
-		
-		  
-		
+
 	}
 }
